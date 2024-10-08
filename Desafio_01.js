054b350345f217b7a3977077269f2ae2a038062b
MGurjao <martinhorneto@gmail.com>
2024-10-07 21:25:21 -0300

    Primeiro Commit

---
+++ Desafio_01.js
@@ -0,0 +1,12 @@
+const nome = "Maria"
+const cpf ="123.123.123-12"
+
+function exibirSobreNome() {
+    let sobreNome = "José"
+    return sobreNome
+}
+
+console.log(nome)
+console.log(exibirSobreNome())
+console.log(cpf)
+console.log(`Meu nome: ${nome}, sobrenome: ${exibirSobreNome()} e CPF: ${cpf}`)
---
+++ Desafio_02.js
@@ -0,0 +1,11 @@
+const produto = "Cadeira"
+const codigoProduto="123CAD"
+
+function exibirCategoriaProduto(){
+    let categoria = "Moveis"
+    return categoria
+}
+
+console.log(produto)
+console.log(exibirCategoriaProduto())
+console.log(codigoProduto)
